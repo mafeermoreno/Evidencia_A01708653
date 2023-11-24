d8d0b34da7bdea75771213d06a6bcd2946c14b66
mafeermoreno <marifermg29@gmail.com>
2023-11-23 18:30:00 -0600

    Add data/domain capa

--- app/src/main/java/com/example/kotlin/examen_mafer/data/ApiRepository.kt
+++ app/src/main/java/com/example/kotlin/examen_mafer/data/ApiRepository.kt
@@ -1,4 +1,21 @@
 package com.example.kotlin.examen_mafer.data
 
-class ApiRepository {
+import com.example.kotlin.examen_mafer.data.network.model.ApiObject
+
+/**
+ * Repository class for fetching COVID-19 data from an API.
+ *
+ * @param apiService An instance of [ApiService] used for making API requests.
+ */
+class ApiRepository(private val apiRepository: ApiRepository) {
+    /**
+     * Retrieves COVID-19 data for a specific country.
+     *
+     * @param country The name of the country for which data is requested.
+     * @return A list of [ApiObject] containing COVID-19 data for the specified country,
+     *         or null in case of an exception.
+     */
+    suspend fun getCovidData(country: String): List<ApiObject>? {
+        return apiRepository.getCovidData(country)
+    }
 }
--- app/src/main/java/com/example/kotlin/examen_mafer/domain/ListRequirement.kt
+++ app/src/main/java/com/example/kotlin/examen_mafer/domain/ListRequirement.kt
@@ -1,4 +1,22 @@
 package com.example.kotlin.examen_mafer.domain
 
-class ListRequirement {
+import com.example.kotlin.examen_mafer.data.ApiRepository
+import com.example.kotlin.examen_mafer.data.network.model.ApiObject
+
+/**
+ * Class representing a list requirement for retrieving COVID-19 data.
+ *
+ * @param apiRepository An instance of [ApiRepository] used for accessing COVID-19 data.
+ */
+class ListRequirement(private val apiRepository: ApiRepository) {
+    /**
+     * Invokes the list requirement to retrieve COVID-19 data for a specific country.
+     *
+     * @param country The name of the country for which data is requested.
+     * @return A list of [ApiObject] containing COVID-19 data for the specified country,
+     *         or null in case of an exception.
+     */
+    suspend operator fun invoke(country: String): List<ApiObject>? {
+        return apiRepository.getCovidData(country)
+    }
 }
